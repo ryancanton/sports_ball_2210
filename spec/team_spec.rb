db800f69112fae7c97342f9f196b77a0fcc46235
Ryan Canton <ryancanton999@gmail.com>
2022-10-25 15:01:39 -0600

    Test: Add test for player_count method

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -20,5 +20,13 @@ RSpec.describe Team do
     expect(team.roster).to eq([player])
   end
 
+  it 'correctly returns the player count' do
+    team = Team.new("Angry Unicorns", "Atlantis")
+    player = Player.new("Tobias Kingler", 1, 36)
+
+    expect(team.player_count).to eq(0)
+    team.add_player(player)
+    expect(team.player_count).to eq(1)
+  end
 
 end
