2dbc918e48ce8755e21fd92c5469e9a5e6104981
Ryan Canton <ryancanton999@gmail.com>
2022-10-25 15:15:13 -0600

    Feat: Add short/long term player, total_value, and details methods for class Team

--- lib/team.rb
+++ lib/team.rb
@@ -18,4 +18,30 @@ class Team
     @roster.push(player)
   end
 
+  def long_term_players
+    long_term_players = roster.map do |player|
+      if (player.contract_length > 24)
+        player
+      end
+    end
+  end
+
+  def short_term_players
+    short_term_players = roster.map do |player|
+      if (player.contract_length <= 24)
+        player
+      end
+    end
+  end
+
+  def total_value
+    value = 0
+    roster.each do |player|
+      value += player.total_cost
+    end
+  end
+
+  def details
+    {"total_value" => self.total_value, "player_count" => self.player_count}
+  end
 end
